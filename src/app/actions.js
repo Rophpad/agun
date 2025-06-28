c1aef05463250fdd5ac7d14244bbfeb2f9c690ef
Bryan <bryan@epitech.eu>
2025-06-28 13:45:25 +0100

    add action

--- src/app/actions.js
+++ src/app/actions.js
@@ -20,3 +20,49 @@ export async function getBalance() {
     throw error;
   }
 }
+
+export async function Invoices() {
+  try {
+    const response = await fetch(`${apiUrl}/api/invoices`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching balance:', error);
+    throw error;
+  }
+}
+
+export async function Invoice(sats, description) {
+  try {
+    const response = await fetch(`${apiUrl}/api/invoice`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        sats: sats,
+        description : description,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching balance:', error);
+    throw error;
+  }
+}
